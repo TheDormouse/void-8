dcf68a9aa43cf1120ca24a777f08a1a551a444d1
Ben Martinez-Cain <benmc@hey.com>
2023-06-26 10:47:04 -0400

    smaller text

--- app/Box.jsx
+++ app/Box.jsx
@@ -1,26 +1,26 @@
 'use client'
 import { useRef } from 'react'
-import {  useFrame } from '@react-three/fiber';
+import { useFrame } from '@react-three/fiber';
 import vertex from './client/shaders/green/shader.vert'
 import fragment from './client/shaders/green/shader.frag'
 import { Text } from '@react-three/drei'
 
-export default function Box({position, color}) {
-    const ref = useRef()
-    useFrame(() => (ref.current.rotation.x = ref.current.rotation.y += 0.001))
-  
-    return (
-     <>
-     <points ref={ref}>
+export default function Box({ position, color }) {
+  const ref = useRef()
+  useFrame(() => (ref.current.rotation.x = ref.current.rotation.y += 0.001))
+
+  return (
+    <>
+      <points ref={ref}>
         <sphereGeometry args={[1, 48, 48]} />
         <pointsMaterial color="white" size={0.015} sizeAttenuation />
-       
+
       </points>
-      <Text color='white' anchorX='center' anchorY='middle' scale={[.2, .2, .2]}>
-            The system is being configured. Please check back later.
-        </Text>
-     </>
-        
-  
-    )
-  }
+      <Text color='white' anchorX='center' anchorY='middle' scale={[.1, .1, .1]}>
+        The system is being configured. Please check back later.
+      </Text>
+    </>
+
+
+  )
+}
