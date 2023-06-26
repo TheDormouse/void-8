4b1c448e0544513d27c29da0635653c34a7e3d45
Ben Martinez-Cain <benmc@hey.com>
2023-06-26 09:36:19 -0400

    under construction

--- app/Box.jsx
+++ app/Box.jsx
@@ -3,17 +3,24 @@ import { useRef } from 'react'
 import {  useFrame } from '@react-three/fiber';
 import vertex from './client/shaders/green/shader.vert'
 import fragment from './client/shaders/green/shader.frag'
+import { Text } from '@react-three/drei'
 
 export default function Box({position, color}) {
     const ref = useRef()
     useFrame(() => (ref.current.rotation.x = ref.current.rotation.y += 0.001))
   
     return (
-     
-      <mesh position={position}  ref={ref} >
-        <boxBufferGeometry  args={[1, 1, 1]} attach="geometry" />
-        <shaderMaterial attach="material" fragmentShader={fragment} vertexShader={vertex} />
-      </mesh>
+     <>
+     <points ref={ref}>
+        <sphereGeometry args={[1, 48, 48]} />
+        <pointsMaterial color="#5786F5" size={0.015} sizeAttenuation />
+       
+      </points>
+      <Text color='white' anchorX='center' anchorY='middle' scale={[.2, .2, .2]}>
+            The system is being configured. Please check back later.
+        </Text>
+     </>
+        
   
     )
   }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+
 import Box from './Box'
 import {Common, View} from './client/View'
 import {view} from './index.module.css'
 
 export default function Home(){
     return(
-    <View orbit className={view}>
+    <View className={view}>
         <Box  position={[0, 0, 0]} />
         <Common />
     </View>
