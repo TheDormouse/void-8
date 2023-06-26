870db913d595adda7242022506e0a245e7bad2f8
Ben Martinez-Cain <benmc@hey.com>
2023-06-26 10:44:52 -0400

    add vscode settings

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,13 @@
 
 import Box from './Box'
-import {Common, View} from './client/View'
-import styles from './index.module.css'
+import { Common, View } from './client/View'
+import { view } from './index.module.css'
 
-export default function Home(){
-    return(
-    <View className={styles.view}>
-        <Box  position={[0, 0, 0]} />
-        <Common />
-    </View>
-        )
+export default function Home() {
+    return (
+        <View className={view}>
+            <Box position={[0, 0, 0]} />
+            <Common />
+        </View>
+    )
 }
