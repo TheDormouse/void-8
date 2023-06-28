7f2008d9e7a4127327ad330697e783b07f5da315
Ben Martinez-Cain <benmc@hey.com>
2023-06-27 22:59:12 -0400

    add has interacted

--- app/Sound.jsx
+++ app/Sound.jsx
@@ -2,18 +2,56 @@
 import { useLoader, useThree } from '@react-three/fiber'
 import { useEffect, useRef, useState } from 'react'
 import * as THREE from 'three'
+
+const useUserInteracted = () => {
+    const [interacted, setInteracted] = useState(false);
+
+    useEffect(() => {
+        const handleInteraction = () => {
+            setInteracted(true);
+            // Remove event listeners after interaction
+            window.removeEventListener('mousemove', handleInteraction);
+            window.removeEventListener('keydown', handleInteraction);
+            window.removeEventListener('touchstart', handleInteraction);
+        };
+
+        // Add event listeners
+        window.addEventListener('mousemove', handleInteraction);
+        window.addEventListener('keydown', handleInteraction);
+        window.addEventListener('touchstart', handleInteraction);
+
+        // Cleanup function
+        return () => {
+            // Remove event listeners on unmount
+            window.removeEventListener('mousemove', handleInteraction);
+            window.removeEventListener('keydown', handleInteraction);
+            window.removeEventListener('touchstart', handleInteraction);
+        };
+    }, []); // Empty dependency array ensures this runs once on mount and not on updates
+
+    return interacted;
+};
+
 export function Sound({ url }) {
+    const hasInteracted = useUserInteracted();
     const sound = useRef()
     const { camera } = useThree()
     const [listener] = useState(() => new THREE.AudioListener())
     const buffer = useLoader(THREE.AudioLoader, url)
     useEffect(() => {
-        sound.current.setBuffer(buffer)
-        sound.current.setRefDistance(1)
-        sound.current.setLoop(true)
-        sound.current.play()
-        camera.add(listener)
-        return () => camera.remove(listener)
-    }, [camera, buffer, listener])
-    return <positionalAudio ref={sound} args={[listener]} />
+        if (hasInteracted) {
+            sound.current.setBuffer(buffer)
+            sound.current.setRefDistance(1)
+            sound.current.setLoop(true)
+            sound.current.play()
+            camera.add(listener)
+            return () => camera.remove(listener)
+        }
+
+    }, [hasInteracted, camera, buffer, listener])
+    return (
+        <>
+            {hasInteracted ? <positionalAudio ref={sound} args={[listener]} /> : null}
+        </>
+    )
 }
