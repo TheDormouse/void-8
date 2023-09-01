1e6ea4b425d76eb6b16e2506b5c04e75b11bf905
Marchy <37279013+TheDormouse@users.noreply.github.com>
2023-09-01 01:51:20 -0400

    Revert "blog"
    This reverts commit 3e378e214550c99268bd4321c419b8aaa25a8e11.

--- app/page.jsx
+++ app/page.jsx
@@ -3,13 +3,14 @@ import { Common, View } from "./client/View";
 import { view } from "./index.module.css";
 import space from "./sounds/SF-bkg-space-loop-1.wav";
 import { Sound } from "./Sound";
+import Face from "./Face";
 
 export default function Home() {
   return (
     <>
       <View className={view}>
         <Box position={[0, 0, 0]} />
-        <Sound url={space} positionalAudioProps={position: [0, 0, 0]} />
+        <Sound url={space} />
         <Common />
       </View>
       <p style={{ color: "white", textAlign: "center", paddingTop: "48vh" }}>
