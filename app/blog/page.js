9986841d06ff8d3dc2615b967a0876206fed8d24
Marchy <madmarchy@live.com>
2023-09-01 01:25:29 -0400

    style

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,9 +1,16 @@
 import { serialize } from "next-mdx-remote/serialize";
 import { promises as fs } from "fs";
 import { MdxContent } from "./mdx-content";
+import { View, Common } from "../client/View";
+import Box from "../Box";
+import { Sound } from "../Sound";
+import space from "../sounds/SF-bkg-space-loop-1.wav";
+import { view } from "../index.module.css";
 
 import path from "path";
 import Link from "next/link";
+import Image from "next/image";
+import profilepicture from "./Subject.png";
 
 async function getAllPostFilenames() {
   // Get all MDX files from the posts directory
@@ -51,18 +58,52 @@ export default async function PostsPage() {
 
   const sortedPosts = sortPostsByDate(posts);
 
+  const sidebarStyles = {
+    position: "fixed",
+    right: "50px", // Adjust this value for the desired padding
+    top: "50%",
+    transform: "translateY(-50%)",
+    border: "1px solid white",
+    borderRadius: "5px",
+    padding: "10px",
+    textAlign: "center",
+    color: "white",
+    width: "200px",
+  };
+
   return (
-    <div style={{ maxWidth: 600, margin: "auto", color: "white" }}>
-      {sortedPosts.map((post, index) => (
-        <div key={index}>
-          <Link href={`/blog/${post.frontmatter.slug}`}>
-            <h1>{post.frontmatter.title}</h1>
-          </Link>
-          <p>Posted: {post.frontmatter.date}</p>
-          <hr />
-          <MdxContent source={post.serialized} />
+    <>
+      <Link href="/">
+        <div
+          style={{ position: "absolute", top: 10, left: 10, cursor: "pointer" }}
+        >
+          ← HOME
         </div>
-      ))}
-    </div>
+      </Link>
+
+      <View className={view}>
+        <Box position={[0, 0, 0]} />
+        <Sound url={space} positionalAudioProps={{ position: [0, 0, 0] }} />
+        <Common />
+      </View>
+
+      <div style={{ maxWidth: 600, margin: "auto", color: "white" }}>
+        {sortedPosts.map((post, index) => (
+          <div key={index}>
+            <Link href={`/blog/${post.frontmatter.slug}`}>
+              <h1>{post.frontmatter.title}</h1>
+            </Link>
+            <p>Posted: {post.frontmatter.date}</p>
+            <hr />
+            <MdxContent source={post.serialized} />
+          </div>
+        ))}
+      </div>
+      <div style={sidebarStyles}>
+        <Image src={profilepicture} width={100} height={100} />
+        <h3>Marchy's Blog</h3>
+        <p>Welcome to my incoherent ramblings. Enjoy</p>
+      </div>
+    </>
   );
 }
